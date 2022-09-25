39b98d0f564ffa4554bd096e663547d853053151
Semenov <semenov@mail.ru>
2022-09-25 19:17:40 +0400

    FRONT-2 Сервис Поиск данных

---
+++ src/service/findData/index.ts
@@ -0,0 +1,12 @@
+import {ObjectAny} from "~/src/model/objectAny";
+
+export function findData(params:ObjectAny){
+    let result = {};
+    for (const key in params) {
+        const elem = params[key];
+        if (elem.value !== undefined){
+            result[key] = elem.value;
+        }
+    }
+    return result;
+}
