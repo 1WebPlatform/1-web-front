b597c5d101f61ff6dfb086aa68569d3d5c635d76
Semenov <semenov@mail.ru>
2022-09-25 19:19:25 +0400

    FRONT-3 Сервис event и callback заполнение

--- src/service/callback/table/loader_table_data.ts
+++ src/service/callback/table/loader_table_data.ts
@@ -1,3 +1,13 @@
-export function LoaderTableData(){
+import {ProceduresApi} from "~/src/api/procedures";
+import {useComponentsStore} from "~/src/store/components";
+import {findData} from "~/src/service/findData";
+import {ProceduresDto} from "~/src/model/proceduresDto";
 
+export async function LoaderTableData(id:number){
+    const store = useComponentsStore();
+    const cms = store.getId(id);
+    const body = findData(cms.event.create[0].params);
+    console.log(body);
+    const dataset = await ProceduresApi(body as ProceduresDto);
+    console.log(dataset);
 }
