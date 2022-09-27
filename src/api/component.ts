0958b577dc4e8695ced0a6808d687ad49a23b70f
Semenov <semenov@mail.ru>
2022-09-27 19:41:55 +0400

    FRONT-1 screen получения

--- src/api/component.ts
+++ src/api/component.ts
@@ -1,5 +1,5 @@
 import { BaseApi } from "~/src/api/index";
 
 export async function ComponentApi(id: number) {
-    return await BaseApi(`screen/${id}`, "component")
+    return await BaseApi(`screen/${id}`, `component-${id}`)
 }
