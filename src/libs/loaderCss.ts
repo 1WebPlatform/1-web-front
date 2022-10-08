b4f8df81fac09c606ef8b00a7c996c2cfe89949f
Semenov <semenov@mail.ru>
2022-10-08 15:55:25 +0400

    FRONT-9 импорт css file в компоненты

--- src/libs/loaderCss.ts
+++ src/libs/loaderCss.ts
@@ -8,12 +8,21 @@ export function loaderCss(id: number) {
     const link_component = `${URL_STATIC}/style/${TypeComponent[cms.id_type]}/${id}.css`;
     const link_template = `${URL_STATIC}/style/template/${TypeComponent[cms.id_type]}.css`;
     loaderLinkCss(link_template);
+
+    /** тут нужно условия если ли файл! */
     loaderLinkCss(link_component);
+    /** тут нужно условия если ли файл! */
 }
 
 function loaderLinkCss(url: string) {
-    const elem = document.createElement('link');
-    elem.rel = "stylesheet";
-    elem.href = url;
-    document.head.append(elem);
+    useHead(()=>{
+        return {
+            link: [
+                {
+                  rel: 'stylesheet',
+                  href: url
+                }
+              ]   
+        }
+    })
 }
