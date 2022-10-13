65a9975d3fdf39fd3abc30f9ef4bf41112a9de5c
Semenov <semenov@mail.ru>
2022-10-13 15:56:32 +0400

    FRONT-6 component button

--- src/component/button/button.ts
+++ src/component/button/button.ts
@@ -1,7 +1,6 @@
 import { Ref } from "vue";
 import { eventBuild } from "~~/src/service/eventComponent/eventBuild";
 import { getCms } from "~~/src/libs/getCms";
-import { saveContextComponent } from "~~/src/libs/saveContextComponent";
 import { ObjectAny } from "~~/src/model/objectAny";
 import { loaderCss } from "~~/src/libs/loaderCss";
 export function button(id: Ref<number>, context: Ref<ObjectAny>) {
